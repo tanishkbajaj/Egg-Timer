4f3d91156d37ea222df4aadd086cd7bb7e3862de
Shubroto <shuvotech25@gmail.com>
2019-07-20 18:22:06 -0500

    adding files

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,12 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var label: UILabel!
+   
+   
+    
+    var timer = Timer()
+    var fraction = 0
+    var isStart = false
+    var isReset = false
+    
+    @IBOutlet weak var Toolbar: UIToolbar!
+    
+    @IBAction func startButton(_ sender: UIBarButtonItem) {
+        
+        if(isStart){
+            stop()
+          
+            
+        }else {
+            start()
+          
+        }
+        
+        
+    }
+    
+    func start(){
+        
+        var item = self.Toolbar.items
+        
+        item![2] = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.pause, target: self, action: #selector(ViewController.startButton(_:)))
+       
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        isStart = true
+        isReset = false
+        
+        self.Toolbar.setItems(item, animated: true)
+        
+        
+        
     }
 
+    func stop(){
+        var item = self.Toolbar.items
+        
+        item![2] = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.play, target: self, action: #selector(ViewController.startButton(_:)))
+       
+        timer.invalidate()
+        isStart = false
+        
+        self.Toolbar.setItems(item, animated: true)
+    }
+   
+    
 
+    
+    
+    @objc func UpdateTimer() {
+        fraction = fraction + 1
+        label.text = String(fraction)
+    }
+    
+    
+    @IBAction func resetButton(_ sender: Any) {
+        var item = self.Toolbar.items
+        
+        item![2] = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.play, target: self, action: #selector(ViewController.startButton(_:)))
+        
+        isStart = false
+        timer.invalidate()
+        fraction = 0
+        label.text = String(fraction)
+       
+        self.Toolbar.setItems(item, animated: true)
+    }
+    
+    @IBAction func addTenMins(_ sender: Any) {
+        fraction = fraction + 10
+        label.text = String(fraction)
+    }
+    
+    
+    @IBAction func subtractTenMins(_ sender: Any) {
+        if(fraction < 10){
+            fraction = 0
+            label.text = String(fraction)
+        }
+        else{
+        fraction = fraction - 10
+        label.text = String(fraction)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
 }
+
 
